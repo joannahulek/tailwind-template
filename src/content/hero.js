504d7d248339c9ba9aeb37f2937870885600bced
Joanna Hulek <joanna.hulek@gmail.com>
2024-05-16 13:12:00 +0200

    updates

--- src/content/hero.js
+++ src/content/hero.js
@@ -1,12 +1,21 @@
 document.write(`
-  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <h2 class="text-xl font-bold mb-2">Joanna Hulek</h2>
-    <p class="text-gray-600 mb-4">Frontend Developer & Scrum Master</p>
-    <a href="https://joannahulek.github.io/" target="_blank">joannahulek.github.io</a>
-    <a href="https://www.linkedin.com/in/joannahulek/" target="_blank">linkedin.com/in/joannahulek</a>
-    <a href="https://github.com/joannahulek" target="_blank">github.com/joannahulek</a>
-    <a href="mailto:my@email.com">my@email.com</a>
-    <a href="tel:+48XXXXXXXXX">+48XXXXXXXXX</a>
-    <p>Summary...</p>
-  </div>
+            <table width="100%">
+    <tr>
+      <th colspan="2"><h1>Joanna Hulek</h1>
+                    <h2>Frontend Developer & Scrum Master</h2>
+                    <a href="https://joannahulek.github.io/" target="_blank">joannahulek.github.io</a></th>
+      <th rowspan="4"><img src="./photo.jpg" alt="IMAGE"/></th>
+    </tr>
+<tr>
+                             <td><a href="https://www.linkedin.com/in/joannahulek/"
+                                target="_blank">linkedin.com/in/joannahulek</a></td>
+                            <td><a href="mailto:my@email.com">my@email.com</a></td>
+                             </tr>
+                             <tr>
+                                <td><a href="https://github.com/joannahulek" target="_blank">github.com/joannahulek</a></td>
+                              <td><a href="tel:+48XXXXXXXXX">+48XXXXXXXXX</a></td>
+                         </tr>
+      <td colspan="2"><p>Summary...</p></td>
+    </tr>
+</table>
 `);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      screens:{
-
-      }
+      screens: {},
     },
   },
   plugins: [],
-}
-
-
+};
