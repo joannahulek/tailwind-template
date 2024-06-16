dfed043f677c2209fa814dab2ca7c36889bf8281
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-16 11:26:22 +0200

    skills refactored and updated

--- src/content/skills.js
+++ src/content/skills.js
@@ -1,6 +1,19 @@
-document.write(`
-  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <h2 class="text-xl font-bold mb-2">Skills</h2>
-    <div class="col-10"><table class="table"><thead><tr><th colspan="2">Software Development</th></tr></thead><tr><td colspan="2"><small><div class="container text-center"><div class="row row-cols-auto"><div class="col">Javascript</div><div class="col">TypeScript</div><div class="col">React</div><div class="col">Bootstrap</div><div class="col">Tailwind CSS</div><div class="col">HTML</div><div class="col">CSS</div><div class="col">JSON</div><div class="col">GIT, GitHub, GitLab</div></div></div></small></td></tr></table><table class="table"><thead><tr><th colspan="2">Soft Skills</th></tr></thead><tr><td colspan="2"><small><div class="container text-center"><div class="row row-cols-auto"><div class="col">Agile</div><div class="col">Kanban</div><div class="col">Scrum</div><div class="col">Public Speaking</div><div class="col">Emotional Intelligence</div><div class="col">Knowledge Sharing</div></div></div></small></td></tr></table></div>
-  </div>
-`);
+function renderSkils() {
+    const container = document.getElementById('skills-container');
+    let content = `
+      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 class="text-xl font-bold mb-2">Skills</h2>
+        <div class="mb-4">
+            <h3 class="text-lg font-bold mb-2">Frontend Development</h3>
+            <p class="text-gray-600 mb-4">• Javascript • TypeScript • React • Bootstrap • Tailwind CSS • HTML • CSS • JSON • GIT, GitHub, GitLab • JIRA •</p>
+        </div>
+        <div class="mb-4">
+            <h3 class="text-lg font-bold mb-2">Soft Skills</h3>
+            <p class="text-gray-600 mb-4">• Agile • Kanban • Scrum •  Public Speaking • Knowledge Sharing • Emotional Intelligence • Empathy •</p>
+        </div>
+        </div>`;
+
+    container.innerHTML = content;
+}
+
+renderSkils();
