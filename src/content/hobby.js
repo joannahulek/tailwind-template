e4742ddb5ff7ab402d953e12f8cfa77f3667f94f
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-21 12:52:51 +0200

    hobbies updated

--- src/content/hobby.js
+++ src/content/hobby.js
@@ -1,6 +1,12 @@
-document.write(`
-  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <h2 class="text-xl font-bold mb-2">Hobby</h2>
-    <p class="text-gray-600 mb-4">dancing, sailing, gardening, books</p> 
-    </div>
-`);
+function renderHobby() {
+    const container = document.getElementById('hobby-container');
+
+    let content = `<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 class="text-xl font-bold mb-2">Hobby</h2>
+                    <p>dancing, sailing, gardening, books</p>
+      </div>
+    `;
+    container.innerHTML = content;
+}
+
+renderHobby();
